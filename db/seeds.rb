f873bce33e8bddfa0418c99126ba35ab3d226e56
Flatiron School <flatironschool@Flatirons-MacBook-Air-8.local>
2018-11-16 09:53:10 -0500

    added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,47 @@
 
 
 User.create(email:"juan@juan.com",username:"Juan",password:"123456")
+
+
 Song.create(artist_name:"Katy Perry", song_name:"Firework", path:"/static/media/firework-katy_perry.80a827eb.lrc")
 Song.create(artist_name:"3 Doors Down", song_name:"Here without you", path:"/static/media/here_without_you-3_doors_down.1d4e92e2.lrc")
 Song.create(artist_name:"Backstreet Boys", song_name:"I want it that way", path:"/static/media/i_want_it_that_way-backstreet_boys.e4aac2c8.lrc")
-Song.create(artist_name:"Miley Cyrus", song_name:"Wrecking ball", path:"/static/media/wrecking_ball-miley_cyrus.f0dafe46.lrc")
+Song.create(artist_name:"Toto", song_name:"Africa", path:"/static/media/africa-toto.1660bc6c.lrc")
+
+Song.create(artist_name:"Ne Yo", song_name:"Because of you", path:"/static/media/because_of_you-ne_yo.2a024107.lrc")
+
+Song.create(artist_name:"Queen", song_name:"Bohemian Rhapsody", path:"/static/media/bohemian_rhapsody-queen.3074ec7b.lrc")
+
+Song.create(artist_name:"George Michael", song_name:"Careless Whisper", path:"/static/media/careless_whisper-george_michael.5ce70d56.lrc")
+
+Song.create(artist_name:"Ty Dolla Sign", song_name:"Clout", path:"/static/media/clout-ty_dolla_sign.b9d57e8f.lrc")
+
+Song.create(artist_name:"ABBA", song_name:"Dancing Queen", path:"/static/media/dancing_queen-abba.d2d38d48.lrc")
+
+Song.create(artist_name:"Cascada", song_name:"Everytime We Touch", path:"/static/media/everytime_we_touch-cascada.f6e7c494.lrc")
+
+Song.create(artist_name:"Alicia Keys", song_name:"If i aint got you", path:"/static/media/if_i_aint_got_you-alicia_keys.164c6b16.lrc")
+
+Song.create(artist_name:"Beyonce", song_name:"Irreplaceable", path:"/static/media/irreplaceable-beyonce.58ba2bda.lrc")
+
+Song.create(artist_name:"Jennifer Lopez", song_name:"Jenny from the block", path:"/static/media/jenny_from_the_block-jennifer_lopez.df7b8226.lrc")
+
+Song.create(artist_name:"Keri Hilson", song_name:"Knock you down", path:"/static/media/knock_you_down-keri_hilson.102e6104.lrc")
+
+Song.create(artist_name:"The Killers", song_name:"Mr Brightside", path:"/static/media/mr_brightside-the_killers.ef45b488.lrc")
+
+Song.create(artist_name:"TLC", song_name:"No Scrubs", path:"/static/media/no_scrubs-tlc.a88893ab.lrc")
+
+Song.create(artist_name:"Billy Joel", song_name:"Piano Man", path:"/static/media/piano_man-billy_joel.526fb38e.lrc")
+
+Song.create(artist_name:"Earth Wind Fire", song_name:"September", path:"/static/media/september-earth_wind_fire.78e52bca.lrc")
+
+Song.create(artist_name:"Adele", song_name:"Set Fire to the Rain", path:"/static/media/set_fire_to_the_rain-adele.ab025d02.lrc")
+
+Song.create(artist_name:"My Chemical Romance", song_name:"Sing", path:"/static/media/sing-my_chemical_romance.62c0176c.lrc")
+
+Song.create(artist_name:"Amy Winehouse", song_name:"Valerie", path:"/static/media/valerie-amy_winehouse.3b6f8d2e.lrc")
+
+Song.create(artist_name:"Maui", song_name:"Your Welcome", path:"/static/media/your_welcome-maui.c260516f.lrc")
+
+Song.create(artist_name:"Cher", song_name:"Believe", path:"/static/media/believe-cher.75a4715b.lrc")
